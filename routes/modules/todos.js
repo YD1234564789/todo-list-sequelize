1b8bda8ad39c66c8964cc6632293d366cf0d59ad
YD1234564789 <a8111202003@gmail.com>
2023-08-07 17:13:46 +0800

    feat: add users & todos function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,11 +3,61 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
+// 跳轉到新增資料頁面
+router.get('/new', (req, res) => {
+  res.render('new')
+})
 
+// 新增資料後重定向回首頁
+router.post('/', (req, res) => {
+  const name = req.body.name
+  const userId = req.user.id
+  return Todo.create({ name, userId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// 跳轉到detail頁面
 router.get('/:id', (req, res) => {
+  const UserId = req.user.id
   const id = req.params.id
-  return Todo.findByPk(id)
+  return Todo.findOne({ where: { id , UserId }})
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+// 跳轉到edit頁面
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.get() }))
+    .catch(error => console.log(error))
+})
+
+// 修改資料
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId }})
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === "on"
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
+
+// 刪除資料
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ id, UserId })
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -21,10 +21,28 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填'})
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符'})
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
+
   User.findOne({ where: { email } }).then(user => {
     if (user) {
-      console.log('User already exists')
+      errors.push({ message: 'Email已註冊過' })
       return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -42,10 +60,13 @@ router.post('/register', (req, res) => {
       .then(() => res.redirect('/'))
       .catch(err => console.log(err))
   })
+  .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
-  res.send('logout')
+  req.logout()
+  req.flash('success_msg', '你已經成功登出。')
+  res.redirect('/users/login')
 })
 
 module.exports = router
