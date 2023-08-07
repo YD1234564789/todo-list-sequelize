ddbac0717e1d40a2c72a02eab10dab57f99941bd
YD1234564789 <a8111202003@gmail.com>
2023-08-07 15:46:27 +0800

    feat: add authenticate function

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const usePassport = require('./config/passport')
+const passport = require('passport')
 const bcrypt = require('bcryptjs')
 const db = require('./models')
 const Todo = db.Todo
@@ -10,8 +13,15 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
+usePassport(app)
+
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -26,18 +36,39 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
 
+
 app.post('/users/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({ name, email, password })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } }).then(user => {
+    if (user) {
+      console.log('User already exists')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
 })
 
 app.get('/users/logout', (req, res) => {
