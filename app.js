2ddaf213a49152182b02eb20e2cb2eb7863dad54
YD1234564789 <a8111202003@gmail.com>
2023-08-07 15:23:07 +0800

    feat: add detail & function

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
-
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 const app = express()
 const PORT = 3000
 
@@ -12,7 +14,12 @@ app.use(express.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.send('hello world')
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 app.get('/users/login', (req, res) => {
@@ -28,14 +35,21 @@ app.get('/users/register', (req, res) => {
 })
 
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 
 app.get('/users/logout', (req, res) => {
   res.send('logout')
 })
 
-
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 
 
 
